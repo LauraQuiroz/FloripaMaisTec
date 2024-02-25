7a6f9c12b2d1624dc909fb004177633559fa3c42
Laura Quiroz <lauquiroz04@gmail.com>
2024-02-25 17:42:04 -0300

    Add actualizar nome do usuario

--- Ex1/script.js
+++ Ex1/script.js
@@ -1,5 +1,22 @@
 // script.js
 
+// Função para atualizar o nome do usuário dinamicamente
+function atualizarNomeUsuario() {
+    // Seleciona o elemento pelo ID
+    var elementoNomeUsuario = document.getElementById("nomeUsuario");
+  
+    // Verifica se o elemento foi encontrado
+    if (elementoNomeUsuario) {
+      // Atualiza o conteúdo do elemento
+      elementoNomeUsuario.textContent = "Laura Patricia Quiroz - Desenvolvedora";
+    }
+  }
+  
+  // Chamando a função ao carregar a página
+  window.onload = function () {
+    // Chama a função para atualizar o nome do usuário
+    atualizarNomeUsuario();
+  };
 function exibirMensagemBoasVindas() {
     alert("Bem-vindo(a), Laura Patricia Quiroz!");
 }

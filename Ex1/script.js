8dbfa77e128872bd64fa98eadc00582e831b3b65
Laura Quiroz <lauquiroz04@gmail.com>
2024-02-25 17:31:32 -0300

    add welcome message

---
+++ Ex1/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+function exibirMensagemBoasVindas() {
+    alert("Bem-vindo(a), Laura Patricia Quiroz!");
+}
+
+// Chamando a função ao carregar a página (opcional)
+window.onload = exibirMensagemBoasVindas;
